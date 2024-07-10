a80a2f0543c33e36ec7a10e5adf76a330db224ed
muhammad-adil <adil.muhammad93@gmail.com>
2024-07-10 12:14:00 +0200

    Vue Meals app Axios add

--- vue-meals-app/src/store/index.ts
+++ vue-meals-app/src/store/index.ts
@@ -1,9 +1,9 @@
 import { createStore } from "vuex";
 
 import state from "./state"
-import actions from "./actions"
-import mutations from "./mutations"
-import getters from "./getters"
+import * as actions from "./actions"
+import * as mutations from "./mutations"
+import * as getters from "./getters"
 
 // Create a new store instance.
 const store = createStore({
--- vue-meals-app/src/views/MealList.vue
+++ vue-meals-app/src/views/MealList.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
-    console.log("Home view")
-    const letters:string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    console.log("MealList view")
+    // const letters:string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 </script>
 
 <template>
     <div class="flex p-8 flex-col items-center justify-center">
-        <div class="flex p-8 justify-center"> 
+        <h2>Meal List</h2>
+        
+        <!-- <div class="flex p-8 justify-center"> 
             <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals" /> 
         </div>
         
         <div class="flex justify-center mt-2 gap-4">
-            <router-link :to="{name : 'byLetter', params : {letter}}" :key="letter" v-for="letter of letters.split('')">
+            <router-link to="/" v-for="letter of letters.split('')">
                 {{ letter }}
             </router-link>
-        </div>
+        </div> -->
+
     </div>
 </template>
 
-
 <style scoped>
-
 </style>
