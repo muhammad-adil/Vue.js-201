eb595e079cfb1fa50d4f708e1bcdb2a88d0c4b85
muhammad-adil <adil.muhammad93@gmail.com>
2024-07-15 11:08:26 +0200

    Vue Weather app initialize and TailwindCSS intall and configure

---
+++ 02-weather-app/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1 class="text-4xl font-bold">appp</h1>
+  </main>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
